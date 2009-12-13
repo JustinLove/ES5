5693d3f2d1edfbde3f82c1d6954f27789c1bc752
Justin Love <hg@JustinLove.name>
2009-12-12 19:45:12 -0600

    filter

--- es5.js
+++ es5.js
@@ -227,7 +227,23 @@ es5.Array = {
 
       return res;
     },
-    filter: null,
+    filter: function(fun /*, thisp*/) {
+      var len = this.length >>> 0;
+      assertCallable(fun);
+
+      var res = new Array();
+      var thisp = arguments[1] || {};
+      for (var i = 0; i < len; i++) {
+        if (i in this) {
+          var val = this[i]; // in case fun mutates this
+          if (fun.call(thisp, val, i, this)) {
+            res.push(val);
+          }
+        }
+      }
+
+      return res;
+    },
     reduce: null,
     reduceRight: null
   }
