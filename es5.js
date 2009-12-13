3eb206112ee6bfe957801b32db18914b57d16b8e
Justin Love <hg@JustinLove.name>
2009-12-12 20:34:26 -0600

    reduce

--- es5.js
+++ es5.js
@@ -244,7 +244,41 @@ es5.Array = {
 
       return res;
     },
-    reduce: null,
+    reduce: function(fun /*, initial*/) {
+      var len = this.length >>> 0;
+      assertCallable(fun);
+
+      // no value to return if no initial value and an empty array
+      if (len == 0 && arguments.length == 1) {
+        throw new TypeError();
+      }
+
+      var i = 0;
+      if (arguments.length >= 2) {
+        var rv = arguments[1];
+      } else {
+        do {
+          if (i in this) {
+            rv = this[i++];
+            break;
+          }
+
+          // if array contains no values, no initial value to return
+          if (++i >= len) {
+            throw new TypeError();
+          }
+        }
+        while (true);
+      }
+
+      for (; i < len; i++) {
+        if (i in this) {
+          rv = fun.call(null, rv, this[i], i, this);
+        }
+      }
+
+      return rv;
+    },
     reduceRight: null
   }
 };
